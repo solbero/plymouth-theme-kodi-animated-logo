337c7b5e26c89af922a8435b4b07e2ed83b18f7c
Njord Solberg <njord.solberg@gmail.com>
2017-05-15 21:48:47 +0200

    Added fakeroot to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-dpkg -b plymouth-theme-kodi-animated-logo
+if ! type "fakeroot" > /dev/null
+then
+	>&2 echo "You must install the package \"fakeroot\" in order to correctly build this .deb file"
+	exit 1
+fi
+
+fakeroot dpkg -b plymouth-theme-kodi-animated-logo
